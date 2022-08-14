e4954955a3146f1b359822e79b2e0d30b8978d54
Ilya Yurchenkov <yurchenkov.99@gmail.com>
2022-08-14 15:28:14 +0300

    Задание 3

---
+++ Example3/Program.cs
@@ -0,0 +1,31 @@
+﻿//  Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//  и проверяет, является ли этот день выходным
+Console.Clear();
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool Weekend (int a)
+{
+    bool result = (a == 6) || (a == 7);
+    return result;
+}
+
+int day = ReadInt("Введите число дня недели от 1 до 7: ");
+
+while (day >= 8)
+{
+    day = ReadInt("Неккоректный ввод, повторите ввод: ");
+}
+
+if (Weekend(day) == true)
+{
+    Console.Write("Выходной");
+}
+else
+{
+    Console.Write("Будний день");
+}
